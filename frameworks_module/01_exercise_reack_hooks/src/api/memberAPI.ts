9f99aaf881e9f97962cf2faad53d4661dd8aae87
Benja Ales <benjinet@gmail.com>
2020-02-07 14:49:17 +0100

    removing mock

--- frameworks_module/01_exercise_reack_hooks/src/api/memberAPI.ts
+++ frameworks_module/01_exercise_reack_hooks/src/api/memberAPI.ts
@@ -2,10 +2,11 @@ import { MemberEntity } from '../model/member';
 import { getMock } from '../model/memberMockData';
 import { promises } from 'dns';
 
-/* const checkStatus = (response: Response): Promise<Response> => {
+ const checkStatus = (response: Response): Promise<Response> => {
   if (response.status >= 200 && response.status < 300) {
     return Promise.resolve(response);
   } else {
+    console.log(response.statusText);
     throw new Error(response.statusText);
   }
 };
@@ -22,22 +23,23 @@ const resolveMembers = (data): Promise<MemberEntity[]> => {
   }));
 
   return Promise.resolve(members);
-}; */
+}; 
 
 const getAllMembers = (organizationName: string): Promise<MemberEntity[]> => {
   const gitHubMembersUrl: string = `https://api.github.com/orgs/${organizationName}/members`;
+
   
-  return getMock(organizationName).then(data => {
+/*   return getMock(organizationName).then(data => {
     if (data) {
       return Promise.resolve(data.member);
     } else {
       return Promise.reject('Not found');
     }
-  });
-  // return fetch(gitHubMembersUrl)
-  //   .then(response => checkStatus(response))
-  //   .then(response => parseJSON(response))
-  //   .then(data => resolveMembers(data));
+  }); */
+  return fetch(gitHubMembersUrl)
+    .then(response => checkStatus(response))
+    .then(response => parseJSON(response))
+    .then(data => resolveMembers(data))
 };
 
 export const memberAPI = {
